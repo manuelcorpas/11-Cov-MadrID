0f98c16769b5cb23b8c32137bb8d3559c73570c3
Manuel <mc@manuelcorpas.com>
2021-05-14 08:30:02 +0000

    bash & perl

--- BASH/00-sam-2-bam.sh
+++ BASH/00-sam-2-bam.sh
@@ -7,6 +7,9 @@ for file in /home/ec2-user/fs1/DATA/SAM/*.sam
 do
     fbname=$(basename "$file" | cut -d. -f1)
     echo "$fbname"
+    if [[ -f $BAM_DIR/$fbname.bam  ]]; then
+        continue
+    fi
     echo "$SAM_DIR/samtools view -S -b $file > $BAM_DIR/$fbname.bam"
     $SAM_DIR/samtools view -S -b $file > $BAM_DIR/$fbname.bam
 done
--- PERL/02-READ-SAMPLES.pl
+++ PERL/02-READ-SAMPLES.pl
@@ -2,34 +2,58 @@ use strict;
 use v5.10; # for say() function
 use Cwd qw(abs_path);
 use Data::Dumper;
+use Spreadsheet::XLSX;
 
 my $abs_path  = abs_path;
-my $indirpath = "$abs_path/../DATA/FASTQ";
+my $indirname = "$abs_path/INPUT/XLS";
+my $infile    = 'EASI_62forParsing.xlsx';
 
-opendir my $dir,$indirpath or die "Cannot open directory: $!";
-my @files = grep {/\.fastq.gz$/} readdir $dir;
-close $dir;
+open my $fh, '<', "$indirname/$infile" or die "can't read open '$infile': $!";
 
-my $FASTQs;
 
-map {
-    my $filename = $_;
-    my ($flowcell,$lane,$index,$read); 
-    if ($filename =~ /(\S+)_(\S+)_(\S+)_(\S+)\.fastq.gz$/) {
-        ($flowcell,$lane,$index,$read) = ($1,$2,$3,$4);
-    }
-    $FASTQs->{$flowcell}{$lane}{$index}{$read}++;
+my ($data,@headings);
+my $i =0;
+my $excel = Spreadsheet::XLSX->new("$indirname/$infile") or die "can't read open '$infile': $!";
 
-} @files;
+my $worksheet = $excel->{Worksheet}[0];
+$worksheet->{MaxRow} ||= $worksheet->{MinRow};
 
-my $i;
-foreach my $flowcell (sort keys %$FASTQs) {
-    foreach my $lane (sort keys %{$FASTQs->{$flowcell}}) {
-        foreach my $index (sort keys %{$FASTQs->{$flowcell}{$lane}}){
-            foreach my $read (sort keys %{$FASTQs->{$flowcell}{$lane}{$index}}) {
-                say ++$i."\t".$flowcell.'_'.$lane.'_'.$index.'_'.$read;
-            }
-        }
-    }
+
+foreach my $row ($worksheet->{MinRow} .. $worksheet->{MaxRow}) {
+  $worksheet->{MaxCol} ||= $worksheet->{MinCol};
+  my ($MaxCol,$MinCol) = ($worksheet->{MaxCol},$worksheet->{MinCol});
+
+
+  foreach my $col ($worksheet->{MinCol} ..  $worksheet->{MaxCol}) {
+    if ($i == 0) {
+      $headings[$col] = $worksheet->{Cells}[$row][$col]->{Val};
+      next;                                                
+                                    
+     }
+     my $cell = $worksheet->{Cells}[$row][$col];
+
+     $data->[$row-1]{$headings[$col]} = $cell->{Val};
+     #printf("( %s , %s    ) => %s\n", $row, $col, $cell->{Val}) if $cell;                                            
+                          
+   }
+   $i++;
+
 }
-say scalar @files . "\tfastq files";
+
+my $sample;
+foreach my $row (@$data) {
+  next if !$row;
+  my $sample_name = $row->{'SAMPLE BARCODE'};
+  my $fastq       = $row->{'NAME'};
+  push @{$sample->{$sample_name}},$fastq;
+}
+
+my $j;
+map {
+  my $sample_name = $_;
+  
+  print ++$j."\t";
+  print join "\t",@{$sample->{$sample_name}};
+  print "\n";
+
+} sort keys %$sample;
